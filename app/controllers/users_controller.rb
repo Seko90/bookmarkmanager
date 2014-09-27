e537a96abbeecc6029d27e4382cc78d661eee946
tilman <tilman@kr23.de>
2014-09-27 19:13:13 +0200

    save user object

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,21 @@
 class UsersController < ApplicationController
 	def new
 		@user = User.new
+    puts @user
 	end
 
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			redirect_to @user,
 			notice: "Ihr Benutzerkonto wurde angelegt"
+      redirect_to user_path
 		else
 			render "new"
 		end
 	end
 
 	def user_params
-		params.require(:user).permit(:email, :password)
+		params.require(:user).permit(:email, :password, :password_confirmation)
 	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 	has_secure_password
 
-	validates :email, :password,
-	format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
-	uniqueness: true
+	validates_presence_of :email, :password
+  validates_uniqueness_of :email
+	#format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
+	#uniqueness: true
 end
